6366ddfb1e8f04a6f1d642c9b120824455f206e8
Ali Zain Malik <alizainmalik123@gmail.com>
2025-03-27 19:51:19 +0500

    Login & Access Token

--- passport-api/app/Http/Controllers/UserController.php
+++ passport-api/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -51,6 +52,68 @@ class UserController extends Controller
             return response()->json([
                 "errors" => $e->getMessage(),
             ]);
+        }
+    }
+
+
+    public function login(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            "email"     =>  "required|email",
+            "password"  =>  "required",
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                "success"   =>  false,
+                "errors"    => $validator->errors(),
+            ], 422);
+        }
+
+        DB::beginTransaction();
+        try 
+        {
+            $user = User::where("email", $request->email)->first();
+            if(empty($user))
+            {
+                return response()->json([
+                    "success"   =>  false,
+                    "message"   =>  "User does not exist",
+                ]);
+            }
+
+            $credentials = [
+                "email"     =>  $request->email,
+                "password"  =>  $request->password,
+            ];
+
+            if(!Auth::attempt($credentials))
+            {
+                return response()->json([
+                    "success"   =>  false,
+                    "message"   =>  "Incorrect email or password",
+                ], 401);
+            }
+
+            Auth::login($user);
+            $access_token = $user->createToken("access_token")->accessToken;
+            DB::commit();
+
+            return response()->json([
+                "success"       =>  true,
+                "access_token"  =>  $access_token,
+                "user"          =>  $user,
+            ], 200);
+
+        } 
+        catch (\Exception $e) 
+        {
+            DB::rollBack();
+            return response()->json([
+                "success"   =>  false,
+                "message"   =>  $e->getMessage(),
+            ], 500);
         }
     }
 }
--- passport-api/routes/api.php
+++ passport-api/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/signup', [UserController::class, "signup"]);
+Route::post("/login", [UserController::class, "login"]);
